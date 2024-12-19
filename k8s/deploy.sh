1b3485a7a125cd207539d5597c0e9d4ccb4db2cb
Ruslan Gasymov <RGasymov@luxoft.com>
2024-12-19 23:44:31 +0400

    Add some comments

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,3 +1,4 @@
+# kubectl apply -f pv.yaml # Only if cluster does not have pv
 helm install mm-config ./mm-config-chart
 helm install -f values-postgres.yaml mm-pstgr ./mm-app-chart
 helm install -f values-backend.yaml mm-back ./mm-app-chart
